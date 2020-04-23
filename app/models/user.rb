9e68fdf68db065dff77bb0617b1c0caecc8f11c9
hirofumi.kinoshita <knfm1245_4344@yahoo.co.jp>
2020-04-23 13:33:28 +0000

    kadai-microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :micropost
   
   def follow(other_user)
     unless self == other_user
@@ -30,4 +32,22 @@ class User < ApplicationRecord
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
+  
+  def favorite(favo_post)
+    self.favorites.find_or_create_by(micropost_id: favo_post.id)
+  end
+  
+  def unfavorite(favo_post)
+    favorite = favorites.find_by(micropost_id: favo_post.id)
+    favorite.destroy if favorite
+  end
+  
+  def favorited?(favo)
+    self.likes.include?(favo)
+  end
+  
+  def favo_microposts
+    Micropost.where(id: self.like_ids)
+  end
+ 
 end
